d4c1524a9b7b0cce71984ab8b20fe6fa0127fbf4
Dan Tovbein <dtovbein@gmail.com>
2023-08-30 13:40:52 -0300

    Move stylesheet

--- functions.php
+++ functions.php
@@ -13,6 +13,6 @@ function enqueue_child_styles() {
 }
 
 add_action('wp_enqueue_scripts', function() {
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/assets/src/scss/custom-popup.scss');
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/custom-popup.css');
 	wp_enqueue_script('custom', get_stylesheet_directory_uri().'/assets/src/js/custom-popup.js');
 });
