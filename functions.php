be81c660ff1286750fdfb2fd0df061af8b2d6481
Dan Tovbein <dtovbein@greenpeace.org>
2024-04-17 17:14:00 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -11,3 +11,54 @@ function enqueue_child_styles() {
 
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation );
 }
+
+wp_register_script( 'render-fixed-donate-button', '' );
+wp_enqueue_script( 'render-fixed-donate-button' );
+wp_add_inline_script(
+    'render-fixed-donate-button',
+    '
+        (() => {
+            document.addEventListener("DOMContentLoaded", () => {
+                const renderOrRemoveMobileDonateButton = () => {
+                    const navMain = document.querySelector("#nav-main");
+
+                    try {
+                        const donateButtonBottomHeader = document.querySelector(".donate-button-bottom-header");
+
+                        if(window.innerWidth < 768) {
+                            if(donateButtonBottomHeader === null) {
+                                const donateButton = navMain
+                                .querySelector(".burger-menu-footer")
+                                .querySelector(".btn-donate")
+                                .cloneNode(true);
+
+                                const donateButtonWrapper = document.createElement("div");
+                                donateButtonWrapper.className = "donate-button-bottom-header";
+                                donateButtonWrapper.style.width = "100%";
+                                donateButtonWrapper.style.textAlign = "center";
+                                donateButtonWrapper.style.padding = "20px 0 20px 0";
+                                donateButtonWrapper.appendChild(donateButton);
+                                const header = document.querySelector("#header");
+                                header.appendChild(donateButtonWrapper);
+                                document.querySelector(".page-content.no-page-title").style.paddingTop = `${header.getBoundingClientRect().height}px`;
+                            }
+                        } else {
+                            if(donateButtonBottomHeader) {
+                                donateButtonBottomHeader.remove();
+                                document.querySelector(".page-content.no-page-title").style.paddingTop = "68px";
+                            }
+                        }
+                    } catch(error) {
+                        // Do nothing
+                    }
+                };
+
+                window.addEventListener("resize", () => {
+                    renderOrRemoveMobileDonateButton();
+                });
+
+                renderOrRemoveMobileDonateButton();
+            });
+        })();
+    ',
+);
